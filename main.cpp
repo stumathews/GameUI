9ed98cbade15b0a94888a03b9d6c7f3ce0c3f764
Stuart Mathews <stuart@stuartmathews.com>
2020-04-14 21:38:58 +0100

    Get it to render a triangle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
 #include <D3DX10.h>
 #include <windows.h>
+#include <dinput.h>
+#include <iostream>
+#include <ostream>
+
+#define KEYPRESSED(buffer, key) (buffer[key] & 0x80)
 
 HINSTANCE g_hInstance = nullptr;
 HWND g_hWindow = nullptr;
@@ -7,6 +14,15 @@ D3D10_DRIVER_TYPE g_d3d10DriverType = D3D10_DRIVER_TYPE_HARDWARE;
 IDXGISwapChain* g_pdx10SwapChain = nullptr;
 ID3D10Device* g_id3dDevice = nullptr;
 ID3D10RenderTargetView* g_pd3d10RenderTargetView = nullptr;
+LPDIRECTINPUTDEVICE8 g_lpInputDevice;
+ID3D10EffectTechnique* g_pd3d10EffectTechnique = nullptr;
+ID3D10Effect* g_id3dEffect = nullptr;
+
+struct TriangleVertex
+{
+	D3DXVECTOR3 Location;
+	D3DXVECTOR3 Color;
+};
 
 LRESULT CALLBACK WndProc(HWND hWindow, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -24,9 +40,48 @@ LRESULT CALLBACK WndProc(HWND hWindow, UINT msg, WPARAM wParam, LPARAM lParam)
 		break;
 	default:
 		return DefWindowProc(hWindow, msg, wParam, lParam);
-
 	}
 	return 0;
+}
+
+// {C1C315B8-E544-4B5C-A7F5-BC1DF0391975}
+static const GUID g_ApplicationGUID = { 0xc1c315b8, 0xe544, 0x4b5c, { 0xa7, 0xf5, 0xbc, 0x1d, 0xf0, 0x39, 0x19, 0x75 } };
+
+
+bool SetupD3DInput()
+{
+	LPDIRECTINPUT8 lpdi;
+	HRESULT hResult;
+
+	hResult = DirectInput8Create(g_hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&lpdi, NULL);
+	if(FAILED(hResult))
+	{
+		// do something
+	}
+
+	hResult = lpdi->CreateDevice(GUID_SysKeyboard,
+		&g_lpInputDevice, NULL);
+	if (FAILED(hResult))
+	{
+		g_lpInputDevice->Release();
+		return FALSE;
+	}
+
+	hResult = g_lpInputDevice->SetDataFormat(&c_dfDIKeyboard);
+	if(FAILED(hResult))
+	{
+		g_lpInputDevice->Release();
+		return FALSE;
+	}
+
+	hResult = g_lpInputDevice->SetCooperativeLevel(g_hWindow, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
+	if(FAILED(hResult))
+	{
+		g_lpInputDevice->Release();
+		return FALSE;
+	}
+
+	g_lpInputDevice->Acquire();
 }
 
 HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
@@ -93,8 +148,7 @@ HRESULT InitD3D10()
 	swap_chain_desc.SampleDesc.Count = 1;
 	swap_chain_desc.OutputWindow = g_hWindow;
 	swap_chain_desc.Windowed = TRUE;
-	
-	
+		
 	HRESULT hResult = D3D10CreateDeviceAndSwapChain(nullptr,
 	                                                g_d3d10DriverType,
 	                                                nullptr,
@@ -111,8 +165,9 @@ HRESULT InitD3D10()
 	if (FAILED(hResult))
 		return hResult;
 
-	hResult = g_id3dDevice->CreateRenderTargetView(pBackBufferSurface, nullptr,
-		&g_pd3d10RenderTargetView);
+	hResult = g_id3dDevice->CreateRenderTargetView(pBackBufferSurface, 
+												nullptr, 
+												&g_pd3d10RenderTargetView);
 
 	pBackBufferSurface->Release();
 
@@ -120,7 +175,8 @@ HRESULT InitD3D10()
 		return hResult;
 
 	g_id3dDevice->OMSetRenderTargets(1,
-		&g_pd3d10RenderTargetView, nullptr);
+										&g_pd3d10RenderTargetView, 
+										nullptr);
 
 	D3D10_VIEWPORT view_port;
 	SecureZeroMemory(&view_port, sizeof(view_port));
@@ -132,18 +188,159 @@ HRESULT InitD3D10()
 	view_port.MinDepth = 0.0f;
 	view_port.MaxDepth = 1.0f;
 
-	g_id3dDevice->RSSetViewports(1, &view_port);
+	g_id3dDevice->RSSetViewports(1,
+								&view_port);
+
+	// make vertex buffer 
+	D3D10_BUFFER_DESC vert_buffer_desc;
+	vert_buffer_desc.Usage = D3D10_USAGE_DEFAULT;
+	vert_buffer_desc.ByteWidth = sizeof(TriangleVertex) * 3;
+	vert_buffer_desc.BindFlags = D3D10_BIND_VERTEX_BUFFER;
+	vert_buffer_desc.CPUAccessFlags = 0;
+	vert_buffer_desc.MiscFlags = 0;
+
+	TriangleVertex array_of_vertex_data[] =
+	{
+		{D3DXVECTOR3(0.0f, 1.0f, 1.0f)},
+		{D3DXVECTOR3(1.0f, -1.0f, 1.0f)},
+		{D3DXVECTOR3(-1.0f, -1.0f, 1.0f)}
+	};
+
+	D3D10_SUBRESOURCE_DATA sub_resource_data;
+	sub_resource_data.pSysMem = array_of_vertex_data;
+	sub_resource_data.SysMemPitch = 0;
+	sub_resource_data.SysMemSlicePitch = 0;
+
+	ID3D10Buffer* vertex_buffer[2] = { nullptr, nullptr };
+	hResult = g_id3dDevice->CreateBuffer(&vert_buffer_desc,
+										&sub_resource_data,
+										&vertex_buffer[0]);
+
+	if (FAILED(hResult))
+		return hResult;
+
+	// make index buffer
+	D3D10_BUFFER_DESC index_buffer_desc;
+	index_buffer_desc.Usage = D3D10_USAGE_DEFAULT;
+	index_buffer_desc.ByteWidth = sizeof(TriangleVertex) * 3;
+	index_buffer_desc.BindFlags = D3D10_BIND_INDEX_BUFFER;
+	index_buffer_desc.CPUAccessFlags = 0;
+	index_buffer_desc.MiscFlags = 0;
+
+	UINT array_of_index_data[] = { 1, 2, 3, 4 };
+
+	D3D10_SUBRESOURCE_DATA index_sub_resource_data;
+	index_sub_resource_data.pSysMem = array_of_index_data;
+	index_sub_resource_data.SysMemPitch = 0;
+	index_sub_resource_data.SysMemSlicePitch = 0;
+
+	ID3D10Buffer* indexBuffer = nullptr;
+	g_id3dDevice->CreateBuffer(&index_buffer_desc, 
+								&index_sub_resource_data,
+								&indexBuffer);
+
+	UINT start_input_slot = 0;
+	UINT number_buffers_in_array = 1;
+	UINT offset_value = 0;
+	UINT stride_value = sizeof(TriangleVertex);
+
+	g_id3dDevice->IASetVertexBuffers(start_input_slot,
+		number_buffers_in_array,
+		vertex_buffer,
+		&stride_value,
+		&offset_value);
+
+	hResult = D3DX10CreateEffectFromFile(L"effect.fx",
+		NULL,
+		NULL,
+		"fx_4_0",
+		D3D10_SHADER_ENABLE_STRICTNESS,
+		0,
+		g_id3dDevice,
+		NULL,
+		NULL,
+		&g_id3dEffect,
+		NULL,
+		NULL);
+
+
+	if (FAILED(hResult))
+		return hResult;
+
+	g_pd3d10EffectTechnique = g_id3dEffect->GetTechniqueByName("Triangle");
+
+	D3D10_PASS_DESC pass_desc;
+	g_pd3d10EffectTechnique->GetPassByIndex(0)->GetDesc(&pass_desc);
+
+	// define input layout
+
+	D3D10_INPUT_ELEMENT_DESC input_layout_desc[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D10_INPUT_PER_VERTEX_DATA, 0 },
+	};
+
+	int number_of_inputs = sizeof(input_layout_desc) / sizeof(input_layout_desc[0]);
+
+	ID3D10InputLayout* p_input_layout = nullptr;
+	hResult = g_id3dDevice->CreateInputLayout(input_layout_desc, 
+												number_of_inputs,
+												pass_desc.pIAInputSignature, 
+												pass_desc.IAInputSignatureSize, 
+												&p_input_layout);
+
+	if (FAILED(hResult))
+		return hResult;
+	
+	
+	g_id3dDevice->IASetInputLayout(p_input_layout);
+	g_id3dDevice->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// Rasterizer state object
+	
+	ID3D10RasterizerState* general_rasterizer_state;
+	D3D10_RASTERIZER_DESC rasterizer_state_desc;
+	g_id3dDevice->CreateRasterizerState(&rasterizer_state_desc,
+		&general_rasterizer_state);
+
+	// depth stencil state object
+	
+	D3D10_DEPTH_STENCIL_DESC depth_stencil_desc;
+	ID3D10DepthStencilState* p_depth_stencil_state;
+	g_id3dDevice->CreateDepthStencilState(&depth_stencil_desc, &p_depth_stencil_state);
+
+	// Blend state object
+	D3D10_BLEND_DESC blend_state_desc;
+	ID3D10BlendState* p_blend_state = nullptr;
+	g_id3dDevice->CreateBlendState(&blend_state_desc, &p_blend_state);
+	g_id3dDevice->OMSetBlendState(p_blend_state, 0, 0xffffffff);
+
+	// sample state object
+
+	D3D10_SAMPLER_DESC sampler_state_desc;
+	ID3D10SamplerState* p_sampler_state = nullptr;
+
+	g_id3dDevice->CreateSamplerState(&sampler_state_desc, &p_sampler_state);
 	
 	return S_OK;
 }
 
+
+
 void RenderScene()
 {
-	float clear_buffer_color[4] = { 1.0f, 0.0f, 0.0f, 1.0f };
+	float clear_buffer_color[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
 
 	g_id3dDevice->ClearRenderTargetView(
 		g_pd3d10RenderTargetView,
 		clear_buffer_color);
+
+	D3D10_TECHNIQUE_DESC technique;
+	g_pd3d10EffectTechnique->GetDesc(&technique);
+	for(int i = 0; i < technique.Passes; i++)
+	{
+		g_pd3d10EffectTechnique->GetPassByIndex(i)->Apply(0);
+		g_id3dDevice->Draw(3, 0);
+	}
 
 	g_pdx10SwapChain->Present(0, 0);
 }
@@ -163,12 +360,32 @@ void DeviceCleanup()
 		g_pdx10SwapChain->Release();
 }
 
+void GetInput()
+{
+	char g_KeyboardBuffer[256];
+	const HRESULT hResult = g_lpInputDevice->GetDeviceState(sizeof(g_KeyboardBuffer), static_cast<LPVOID>(&g_KeyboardBuffer));
+	if (FAILED(hResult))
+	{
+		// Let do the worst thing and assume its ok and this will not be run. Bad.
+		return;
+	}
+	
+	if (KEYPRESSED(g_KeyboardBuffer, DIK_SPACE))
+	{
+		std::cout << "S key was pressed" << std::endl;
+	}
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
-	if (FAILED(InitWindow(hInstance, nCmdShow)))
+	if (FAILED(InitWindow(hInstance, nCmdShow))) 
+	{
+		DeviceCleanup();
 		return 0;
+	}
 
 	InitD3D10();
+	SetupD3DInput();
 	
 	MSG msg = { nullptr};
 	while (WM_QUIT != msg.message)
@@ -180,6 +397,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 		}
 		else
 		{
+			GetInput();
 			RenderScene();
 		}
 	}
